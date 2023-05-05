a4cb53921d0894e1b95d0e744394f2c8d3b0b4b2
Haim Kasel <haim.kasel@compiralabs.com>
2023-05-05 14:38:24 +0300

    add utils package

--- tests/test_wiki_scraper.py
+++ tests/test_wiki_scraper.py
@@ -1,9 +1,11 @@
 import os
+import sys
 
 import pytest
 
 from utils.html_builder import DictHtmlBuilder
 from utils.wiki_animals_scraper import WikiScraper
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 VALID_URL = "https://en.wikipedia.org/wiki/List_of_animal_names"
 INVALID_URL = "https://en.wikipedia.org/wiki/List_of_animal_namesaaa"
