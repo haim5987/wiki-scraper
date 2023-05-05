f1cd33c55d76ee3c2db2863b399b862c7c4c2548
Haim Kasel <haim.kasel@compiralabs.com>
2023-05-05 13:13:38 +0300

    list all animals

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from utils.html_builder import DictHtmlBuilder
 from utils.utils_functions import *
 import concurrent.futures
 
+
+def display_adj_animal_dict_and_lcl_links(adj_animal_dict):
+    for adjective in adj_animal_dict.keys():
+        print(f"List of Animals of {adjective}:")
+        for animal in adj_animal_dict[adjective]:
+            print(f'{animal}: {TMP_PATH}/{animal}.jpg')
+
+
 if __name__ == '__main__':
     wiki_scraper = WikiScraper(f'{WIKI_URL}{WIKI_ANIMAL_LIST_PATH}')
     wiki_scraper.init_species_table()
@@ -13,11 +21,10 @@ if __name__ == '__main__':
         img_saver_task = executor.submit(wiki_scraper.save_animals_images)
 
         animal_adj_dict = animal_adj_dict_task.result()
-        print(animal_adj_dict)
 
         converter = DictHtmlBuilder(animal_adj_dict)
         converter.export_dict_to_html(INDEX_HTML)
 
         img_result = img_saver_task.result()
-        print_dir_content(TMP_PATH)
-
+        # print_dir_content(TMP_PATH)
+        display_adj_animal_dict_and_lcl_links(animal_adj_dict)
